0816423ff799497762569c083203828210c791f3
Lance Soliman <lanson619@gmail.com>
2025-07-10 16:45:31 -0700

    reset app

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,8 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
-  return (
+  
+return (
     <div
       className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
