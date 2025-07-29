69e5efeb430ed12878e0606af68fff2e14393042
Lance Soliman <lanson619@gmail.com>
2025-07-28 22:08:04 -0700

    update projects, skills, contact

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,11 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
-
